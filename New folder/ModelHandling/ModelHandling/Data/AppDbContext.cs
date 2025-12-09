ee622db1b09437c0f4fae5cece83fea40fc6d722
TaimoorQadri123 <dchota374@gmail.com>
2025-12-08 23:28:52 -0800

    done

---
+++ New folder/ModelHandling/ModelHandling/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelHandling.Models;
+
+namespace ModelHandling.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
+        {
+        }
+
+
+        public DbSet<ModelHandling.Models.Students> Students { get; set; }
+        public DbSet<ModelHandling.Models.Category> Category { get; set; } = default!;
+        public DbSet<ModelHandling.Models.Products> Products { get; internal set; }
+    }
+}
---
+++ New folder/ModelHandling/ModelHandling/Models/Products.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ModelHandling.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductCatDescription { get; set; }
+
+        public double Price { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+
+        [ValidateNever]
+
+        public Category Category { get; set; }
+
+
+
+            
+
+
+    }
+}
